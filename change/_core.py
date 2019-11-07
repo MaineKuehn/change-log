c7224ef9d5a9348842bb02afe6889c97962abea4
Max Fischer <maxfischer2781@gmail.com>
2019-11-07 14:49:44 +1030

    initial commit

---
+++ change/_core.py
@@ -0,0 +1,16 @@
+import argparse
+
+from . import log
+
+CLI = argparse.ArgumentParser(
+    description='change and manage package information'
+)
+# TODO: add pyproject.toml defaults support
+CMD_CLI = CLI.add_subparsers(required=True, dest="CMD")
+
+log.mount_cli(CMD_CLI)
+
+
+def main():
+    options = CLI.parse_args()
+    options.__command__(options)
