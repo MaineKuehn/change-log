dabdfd257aa3f46ca221374ff1a584b9b6b6a7cb
Max Fischer <maxfischer2781@gmail.com>
2019-11-07 23:34:01 +1030

    parser can read commands from files

--- change/_core.py
+++ change/_core.py
@@ -1,9 +1,18 @@
 import argparse
+import shlex
 
 from . import log
 
-CLI = argparse.ArgumentParser(description="change and manage package information")
-# TODO: add pyproject.toml defaults support
+
+class ConfigArgumentParser(argparse.ArgumentParser):
+    def convert_arg_line_to_args(self, arg_line):
+        return shlex.split(arg_line, comments=True)
+
+
+CLI = ConfigArgumentParser(
+    description="change and manage package information",
+    fromfile_prefix_chars="@",
+)
 CMD_CLI = CLI.add_subparsers(required=True, dest="CMD")
 
 log.mount_cli(CMD_CLI)
